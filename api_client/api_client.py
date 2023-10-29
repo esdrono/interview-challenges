12bc19292814e9440c5bdd352b12dd89a9781ca9
esdrono <77854186+esdrono@users.noreply.github.com>
2023-10-29 00:04:57 -0600

    Update api_client.py

--- api_client/api_client.py
+++ api_client/api_client.py
@@ -1,22 +1,23 @@
 import requests
+import json
 import yaml
 
 def read_todo_file(filename: str):
-    todos = []
-    with open(filename, 'rb') as file
-        todos = yaml.safe_load(file)
+    with open(filename, 'r') as file:
+        todos = json.load(file)
     return todos
 
 def get_todo_list(ids: list):
     todos = []
-    ## TODO: implement this
+    for id in ids:
+        todo = get_todo(id)
+        todos.append(todo)
     return todos
 
 def get_todo(id: int):
-    r = requests.get("https://jsonplaceholder.typicode.com/todos/{id}")
+    r = requests.get(f"https://jsonplaceholder.typicode.com/todos/{id}")
     return r.json()
 
 def get_user(id: int):
-    r = requests.get("https://jsonplaceholder.typicode.com/users/{id}")
+    r = requests.get(f"https://jsonplaceholder.typicode.com/users/{id}")
     return r.json()
-
