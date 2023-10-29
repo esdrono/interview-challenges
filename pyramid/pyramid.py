1c68201975701880442c055ec07fda36d78c7ee4
esdrono <77854186+esdrono@users.noreply.github.com>
2023-10-28 23:48:55 -0600

    Update pyramid.py

--- pyramid/pyramid.py
+++ pyramid/pyramid.py
@@ -1,14 +1,18 @@
-
 def get_max_blocks(height: int):
     blocks = 0
-    blocks_in_level = 0
-    for h in height:
-        blocks_in_level += 1
-        blocks += blocks_in_level
+    for h in range(1, height + 1):  # Fix here: `height` should be integer, not iterable
+        blocks += h
     return blocks
 
 def get_min_blocks(height: int):
-    pass
+    # In a pyramid, the minimum blocks required are the same as the maximum blocks
+    return get_max_blocks(height)
 
 def get_height(blocks: int):
-    pass
+    height = 0
+    while blocks > 0:
+        height += 1
+        blocks -= height
+    if blocks < 0:
+        return height - 1
+    return height
